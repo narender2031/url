74e676c24b0fed48a8f892d4457798dcd3dd250b
narender <narender2031@gmail.com>
2018-06-11 15:03:18 +0530

    update

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -17,4 +17,41 @@ class SessionController < ApplicationController
         end
        
     end
+    def register
+        @user = User.new
+    end
+
+    def create_register
+        @user_email =  User.find_by(email: user_params[:email])
+        @user_phone = User.find_by(phone: user_params[:phone])
+        if @user_email
+          flash[:notice] = "This email is taken by another user"
+          redirect_to register_path
+        elsif @user_name
+          flash[:notice] = "User name is taken by another user"
+          redirect_to register_path
+        elsif @user_phone 
+          flash.notice = "Phone number is taken by another user"
+          redirect_to register_path
+        elsif 
+          @user = User.new
+          @user.name = user_params[:name]
+          @user.phone = user_params[:phone]
+          @user.email = user_params[:email]
+          @user.password = user_params[:password]
+          if @user.save
+              session[:user_id] = @user.id
+              cookies[:token] = @user.token
+              # puts "Hello"
+              redirect_to dash_path
+          else
+              flash[:notice] = "email and password are incorrect"
+              redirect_to register_path
+          end
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :phone, :user_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/login', to: "session#index"
   post '/login', to: "session#create"
   get '/dash', to: "url_short#index"
+  get '/register', to: "session#register"
+  post '/register', to: "session#create_register"
   post '/short_url', to: "url_short#create"
 end
